6b5f6d7b283159cd282dfca50178989669c9d217
Ben Zenker <imbenzenker@gmail.com>
2017-05-15 21:26:11 -0400

    base app component

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// Third Party Imports
+import React from 'react'
+import { render } from 'react-dom'
+
+// Import root app
+import App from './containers/App'
+
+// Render the app into the DOM
+render(<App />, document.getElementById('app'))
---
+++ app/containers/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Cat Meow Side</h1>
+      <h3>How Bow Dat</h3>
+    </div>
+  )
+}
