140e8db1ff86e066c683fa74beba1c939a3926eb
Ben Zenker <imbenzenker@gmail.com>
2017-05-16 05:38:12 -0400

    custom config for phaser

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path')
 
+// Phaser webpack config
+const phaserModule = path.join(__dirname, '/node_modules/phaser/')
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
+const pixi = path.join(phaserModule, 'build/custom/pixi.js')
+const p2 = path.join(phaserModule, 'build/custom/p2.js')
+
 module.exports = {
   entry: [
     './app/app.js'
@@ -20,10 +26,36 @@ module.exports = {
             presets: ['env', 'es2015', 'react']
           }
         }
+      },
+      {
+        test: /pixi\.js/,
+        use: {
+          loader: 'expose-loader',
+          options: 'PIXI'
+        }
+      },
+      {
+        test: /phaser-split\.js/,
+        use: {
+          loader: 'expose-loader',
+          options: 'Phaser'
+        }
+      },
+      {
+        test: /p2\.js/,
+        use: {
+          loader: 'expose-loader',
+          options: 'p2'
+        }
       }
     ]
   },
   resolve: {
     modules: ['app', 'node_modules'],
+    alias: {
+      'phaser': phaser,
+      'pixi': pixi,
+      'p2': p2
+    }
   }
 }
