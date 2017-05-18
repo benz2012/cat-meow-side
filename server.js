bb68555c7363ac078fa52ccdaa14ad98b8d0de32
Ben Zenker <imbenzenker@gmail.com>
2017-05-18 10:44:46 -0400

    redirect http requests to https in the node server

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const express = require('express')
 
 // Global variables
 const app = express()
+const env = process.env.NODE_ENV || 'development'
+
+// HTTP to HTTPS redirect
+const forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(301, ['https://', req.get('Host'), req.url].join(''))
+  } else {
+    next()
+  }
+}
 
 // Routes
+if (env === 'production') {
+  app.use(forceSSL)
+}
 app.use(express.static('dist'))
 
 // Listen
